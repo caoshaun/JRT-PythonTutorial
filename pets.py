c62d759f11a9ad57dfad5ceb705e72f29bca59d5
cao-shawn <37239848+caoshaun@users.noreply.github.com>
2018-03-11 22:55:06 +0800

    Add files via upload

---
+++ pets.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+def describe_pet(animal_type,pet_name):
+    """显示宠物的信息"""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title()+ ".")
+
+describe_pet('cat', 'miaomiao')
+input()
